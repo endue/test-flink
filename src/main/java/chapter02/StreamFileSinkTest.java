1b761e970f70f37b45069abbded7cddef30f842e
meng.li1 <meng.li1@qingteng.cn>
2022-09-10 13:57:01 +0800

    增加sink操作

---
+++ src/main/java/chapter02/StreamFileSinkTest.java
@@ -0,0 +1,14 @@
+package chapter02;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * @author meng.li1
+ * @Date 2022/9/10 13:18
+ * @Description ：
+ */
+public class StreamFileSinkTest {
+    public static void main(String[] args) {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+    }
+}
---
+++ src/main/java/chapter02/TransformationJava.java
@@ -0,0 +1,25 @@
+package chapter02;
+
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * @author meng.li1
+ * @Date 2022/9/9 13:06
+ * @Description ：{@link https://nightlies.apache.org/flink/flink-docs-release-1.14/docs/dev/datastream/operators/overview/}
+ */
+public class TransformationJava {
+
+    public static void main(String[] args) {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStreamSource<String> dataStreamSource = env.fromElements("a", "b", "c");
+
+        SingleOutputStreamOperator<String> singleOutputStreamOperator = dataStreamSource.map(s -> s.toUpperCase());
+
+        
+
+    }
+
+}
